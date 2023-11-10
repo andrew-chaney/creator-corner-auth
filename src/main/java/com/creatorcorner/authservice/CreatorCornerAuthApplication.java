7228e7b904713da9765d83fa0a4ef02a73b3eec4
andrew-chaney <andrewchaney24@gmail.com>
2023-11-09 21:54:08 -0700

    base spring project

---
+++ src/main/java/com/creatorcorner/authservice/CreatorCornerAuthApplication.java
@@ -0,0 +1,13 @@
+package com.creatorcorner.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorCornerAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatorCornerAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/creatorcorner/authservice/CreatorCornerAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creatorcorner.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorCornerAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
